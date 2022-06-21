b1980de2d5ca2791416df035f26d23585c2dc29d
sy Kim <48751536+youngyoungsunny@users.noreply.github.com>
2022-06-21 22:56:30 +0900

    hash_복습 필요

--- programmers/lv1_완주하지못한 선수.py
+++ programmers/lv1_완주하지못한 선수.py
@@ -1,4 +1,4 @@
-#1 : 효율성에서 다 틀림. 
+#1 : TestCase 정답 | 효율성 오답 
 def solution(participant, completion):
     answer = ''
     
@@ -14,9 +14,32 @@ def solution(participant, completion):
     return answer
 
 
-# 문제점 : for문안에 in으로 O(n^2) / 또한  remove로 인해 O(n2) , 조건문에 in이 있어서 O(n2)에 걸림.  sort 후 해결  
-#2 : 
+# 문제점 : for문 및 for문안에서 in으로 인해 O(n^2) --> 효율성 오답 
+#         또한  if내에서 remove로 인해 O(n^2)   
 
+
+#2 : dictionary를 사용하여 효율성 해결
+def solution(participant, completion):
+    hash = {} #dictionary {key : value }
+    
+    answer = ''
+    
+    for person in participant : 
+        if person in hash : 
+            hash[person] += 1 #중복 체크
+    
+        else : 
+            hash[person] = 1 #완주자 체크
+    
+    for person in completion : 
+        if hash[person] == 1 : #완주자 체크 
+            del hash[person]
+        else : 
+            hash[person] -= 1
+    
+    answer = list(hash.keys())[0]
+    
+    return answer
 import collections
 def solution(participant, completion):
     answer = collections.Counter(participant) - collections.Counter(completion)
